97f4a73362664b52860c686c7359203961b48708
hrittik16 <hrittikdas16@gmail.com>
2020-03-23 15:53:14 +0530

    max_pairwise_product.cpp updated

--- Algorithmic Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ Algorithmic Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
+long long MaxPairwiseProduct(const std::vector<int>& numbers) {
+    long long max_product = 0;
     int n = numbers.size();
 
     for (int first = 0; first < n; ++first) {
         for (int second = first + 1; second < n; ++second) {
             max_product = std::max(max_product,
-                numbers[first] * numbers[second]);
+                (long long)numbers[first] * (long long)numbers[second]);
         }
     }
 
@@ -33,7 +34,27 @@ long long MaxPairwiseProductFast(const std::vector<int>& numbers) {
 	return ((long long)largest*(long long)second_largest);
 }
 
+void stress_test() {
+	while(1) {
+		int n = rand()%10 + 2;
+		std::cout << "n = " << n << "\n";
+		std::vector<int> a;
+		for(int i = 0; i < n; i++)
+			a.push_back(rand()%100000);
+		long long ans1 = MaxPairwiseProduct(a);
+		long long ans2 = MaxPairwiseProductFast(a);
+		if(ans1 != ans2) {
+			std::cout << "Wrong Answer!\n";
+			std::cout << "ans1 = " << ans1 << "\n";
+			std::cout << "ans2 = " << ans2 << "\n";
+			break;
+		}
+		else std::cout << "Correct answer\n";
+	}
+}
+
 int main() {
+	//stress_test();
     int n;
     std::cin >> n;
     std::vector<int> numbers(n);
