1253484ff1869e0619952e28ee9e58c893cbba97
hrittik16 <hrittikdas16@gmail.com>
2020-03-23 20:58:57 +0530

    Greatest common divisor updated

--- Algorithmic Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
+++ Algorithmic Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <cassert>
 
 int gcd_naive(int a, int b) {
   int current_gcd = 1;
@@ -12,7 +14,28 @@ int gcd_naive(int a, int b) {
   return current_gcd;
 }
 
+int gcd_fast(int a, int b) {
+  if(a == 0)
+    return b;
+  return gcd_fast(b%a, a);
+}
+
+int test() {
+  while(1) {
+    int a = rand()%1000000000;
+    int b = rand()%1000000000;
+    if(gcd_naive(a, b) != gcd_fast(a, b)) {
+      std::cout << "Wrong Answer!\n";
+      std::cout << "a = " << a << " b = " << b << "\n";
+      break;
+    }
+    else
+      std::cout << "Correct answer\n";
+  }
+}
+
 int main() {
+  //test();
   int a, b;
   std::cin >> a >> b;
   std::cout << gcd_naive(a, b) << std::endl;
